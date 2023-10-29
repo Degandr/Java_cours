2bd555503b7ccdd3ebdd825acb7bb11046facfbc
Andrew <freenick96@gmail.com>
2023-10-29 17:22:23 +0100

    hw_3

---
+++ src/test/java/Task_1.java
@@ -0,0 +1,17 @@
+public class Task_1 {
+    public String name;
+    public String surname;
+    public String position;
+    public int salary;
+
+    public Task_1 (String name, String surname, String position, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public void info () {
+        System.out.println("My first name is "+ name + ".\n My last name is " + surname + ".\n My position is "+ position+ ".\n My salary is " + salary+"\n");
+    }
+}
---
+++ src/test/java/Task_2.java
@@ -0,0 +1,23 @@
+public class Task_2 {
+    int speed;
+    double timeAcceleration;
+    double timeBraking ;
+    final String brand = "Audi";
+
+    public Task_2 (int speed, double timeAcceleration, double timeBraking) {
+        this.speed = speed;
+        this.timeAcceleration = timeAcceleration;
+        this.timeBraking = timeBraking;
+    }
+    public void getName() {
+        System.out.println(brand);
+    }
+
+    public void getDifferenceBetweenTimeBrakingAndTimeAcceleration () {
+        System.out.println(timeAcceleration - timeBraking);
+    }
+
+    public void isTimeAccelerationEqualTimeBraking() {
+        System.out.println(timeAcceleration == timeBraking);
+    }
+}
