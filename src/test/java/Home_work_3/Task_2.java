eda9a2da9e966aacb84fd64cecfd5b49f3c8f474
Andrew <freenick96@gmail.com>
2023-10-31 13:54:26 +0100

    fix_formatting

--- src/test/java/Home_work_3/Task_2.java
+++ src/test/java/Home_work_3/Task_2.java
@@ -3,19 +3,20 @@ package Home_work_3;
 public class Task_2 {
     int speed;
     double timeAcceleration;
-    double timeBraking ;
+    double timeBraking;
     final String brand = "Audi";
 
-    public Task_2 (int speed, double timeAcceleration, double timeBraking) {
+    public Task_2(int speed, double timeAcceleration, double timeBraking) {
         this.speed = speed;
         this.timeAcceleration = timeAcceleration;
         this.timeBraking = timeBraking;
     }
+
     public void getName() {
         System.out.println(brand);
     }
 
-    public void getDifferenceBetweenTimeBrakingAndTimeAcceleration () {
+    public void getDifferenceBetweenTimeBrakingAndTimeAcceleration() {
         System.out.println(timeAcceleration - timeBraking);
     }
 
