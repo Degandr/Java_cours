3f9621f022b1ba07ccfe5da73519b4f3f9dbd93f
Andrew <freenick96@gmail.com>
2023-12-21 20:20:23 +0100

    hw_23(restAssured_response)

---
+++ src/test/java/Home_work_23/Models/PostPutPatchMethodsModel.java
@@ -0,0 +1,13 @@
+package Home_work_23.Models;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PostPutPatchMethodsModel {
+    private String name;
+    private String job;
+}
